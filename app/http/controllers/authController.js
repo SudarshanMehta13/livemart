65871ff5e4a68b8fd3cc4cf72fdd02550f26286e
SudarshanMehta13 <f20180579@hyderabad.bits-pilani.ac.in>
2021-04-14 22:44:30 +0530

    minor changes

--- app/http/controllers/authController.js
+++ app/http/controllers/authController.js
@@ -41,7 +41,7 @@ function authController() {
             res.render('auth/register')
         },
         async postRegister(req, res) {
-         const { name, email, password,location }   = req.body
+         const { name, email, password }   = req.body
          // Validate request 
          if(!name || !email || !password ) {
              req.flash('error', 'All fields are required')
@@ -49,8 +49,6 @@ function authController() {
              req.flash('email', email)
             return res.redirect('/register')
          }
-         
-        
          // Check if email exists 
          User.exists({ email: email }, (err, result) => {
              if(result) {
