63f0445b482adf484fa30d6c1385bbc84ee92e38
SudarshanMehta13 <f20180579@hyderabad.bits-pilani.ac.in>
2021-06-24 21:25:15 +0530

    deploy

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.use((req, res, next) => {
 // set Template engine
 app.use(expressLayout) // express will now know to use ejs template engine
 app.set('views', path.join(__dirname, '/resources/views'))
-app.get('*',(req,res)=>res.sendFile(path.join(__dirname, '/resources/views')))
+app.get('*',(req,res)=>res.sendFile(path.join(__dirname, '/resources/views/layout.ejs')))
 app.set('view engine', 'ejs')
 
 require('./routes/web')(app)
